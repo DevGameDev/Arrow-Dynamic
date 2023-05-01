94e5809f2267d9d8ed3ac0d31c8899d94454573e
zacharyweisenbloom <38918997+zacharyweisenbloom@users.noreply.github.com>
2023-05-01 14:50:06 -0700

    added weapon wheel

---
+++ Arrow-Dynamic/Assets/Scripts/WeaponWheelButtonController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WeaponWheelButtonController : MonoBehaviour
+{
+    public int ID;
+    public string itemName;
+    public TextMeshProUGUI itemText;
+    public Image selectedItem; 
+    public Sprite icon;
+
+    private Animator anim;
+    private bool selected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(selected){
+            selectedItem.sprite = icon;
+            itemText.text = itemName;
+        }
+    }
+
+
+    public void Selected()
+    {
+        selected = true;
+        WeaponWheelController.weaponID = ID;
+
+    }
+
+    public void Deselected(){
+        selected = false;
+        WeaponWheelController.weaponID = 0;
+        
+    }
+
+    public void HoverEnter(){
+        anim.SetBool("Hover", true);
+        itemText.text = itemName;
+    }
+
+    public void HoverExit(){
+        anim.SetBool("Hover", false);
+        itemText.text = "";
+    }
+
+}
