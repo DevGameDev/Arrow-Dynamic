b2eb09d355850dd61d6baa637fdad6502d5a864b
unknown <dch@uoregon.edu>
2023-05-02 00:20:16 -0700

    test animation

--- Arrow-Dynamic/Assets/Scripts/PlayerBow.cs
+++ Arrow-Dynamic/Assets/Scripts/PlayerBow.cs
@@ -7,11 +7,27 @@ public class PlayerBow : MonoBehaviour
     public GameObject arrowPrefab;
     public Transform arrowSpawnPoint;
     public Transform cameraTransform;
+    public Transform handTransform;
+    private Transform arrowTransform;
+    public Transform bowTransform;
     public float arrowSpeed = 50.0f;
     public float arrowReadyTime = 0.25f;
     public float maxPullTime = 2.0f;
 
+    public Vector3 handRestPosition;
+    public Vector3 handMinPullPosition;
+    public Vector3 handMaxPullPosition;
+    public Vector3 arrowRestPosition;
+    public Vector3 arrowPullPosition;
+    public Vector3 arrowRestRotation;
+    public Vector3 arrowPullRotation;
+    public Vector3 bowRestPosition;
+    public Vector3 bowPullPosition;
+    public Vector3 bowRestRotation;
+    public Vector3 bowPulledRotation;
+
     private float currentPullTime = 0.0f;
+    private GameObject currentArrow = null;
     private bool isArrowReady = false;
     private bool isBowPulled = false;
 
@@ -23,6 +39,9 @@ public class PlayerBow : MonoBehaviour
             {
                 isBowPulled = true;
                 currentPullTime = 0.0f;
+                currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, cameraTransform.rotation);
+                arrowTransform = currentArrow.transform;
+                arrowTransform.parent = cameraTransform;
             }
             else if (currentPullTime > arrowReadyTime) isArrowReady = true;
 
@@ -30,7 +49,6 @@ public class PlayerBow : MonoBehaviour
             currentPullTime += Time.deltaTime;
             currentPullTime = Mathf.Clamp(currentPullTime, 0, maxPullTime);
 
-            UpdateBowPullAnimation(currentPullTime);
         }
         // Check if the player releases the left mouse button
         else if (isArrowReady)
@@ -38,24 +56,49 @@ public class PlayerBow : MonoBehaviour
             isArrowReady = false;
             isBowPulled = false;
 
-            // Spawn an arrow and shoot it
-            GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, cameraTransform.rotation);
-            Rigidbody arrowRigidbody = arrow.GetComponent<Rigidbody>();
-
-            Vector3 shootDirection = (cameraTransform.forward + transform.forward).normalized;
-
+            // Shoot the arrow
+            Rigidbody arrowRigidbody = currentArrow.GetComponent<Rigidbody>();
+            arrowRigidbody.isKinematic = false;
+            arrowRigidbody.useGravity = true;
+            arrowTransform.parent = null;
+            Vector3 shootDirection = cameraTransform.forward;
             arrowRigidbody.AddForce(shootDirection * currentPullTime * arrowSpeed, ForceMode.Impulse);
+
+            // Reset pull time
+            currentPullTime = 0.0f;
         }
         // Check if the player cancels the shot by pressing the right mouse button
         else if (Input.GetMouseButton(1))
         {
             isBowPulled = false;
             currentPullTime = 0.0f;
-            UpdateBowPullAnimation(currentPullTime);
+
+            if (currentArrow != null)
+            {
+                // Destroy the arrow and remove the reference
+                Destroy(currentArrow);
+                currentArrow = null;
+            }
         }
+        float test = 2.0f * Mathf.Abs(Mathf.Sin(Time.time));
+
+        UpdateBowPullAnimation(test);
     }
 
-    void UpdateBowPullAnimation(float pullDistance)
+    void UpdateBowPullAnimation(float pullTime)
     {
+        if (currentArrow == null) return;
+
+        float pullRatio = pullTime / maxPullTime;
+
+        if (pullRatio < 0.5f)
+            handTransform.localPosition = Vector3.Lerp(handRestPosition, handMinPullPosition, pullRatio * 2);
+        else
+            handTransform.localPosition = Vector3.Lerp(handMinPullPosition, handMaxPullPosition, (pullRatio - 0.5f) * 2);
+
+        arrowTransform.localPosition = Vector3.Lerp(arrowRestPosition, arrowPullPosition, pullRatio);
+        arrowTransform.localRotation = Quaternion.Lerp(Quaternion.Euler(arrowRestRotation), Quaternion.Euler(arrowPullRotation), pullRatio);
+        bowTransform.localPosition = Vector3.Lerp(bowRestPosition, bowPullPosition, pullRatio);
+        bowTransform.localRotation = Quaternion.Lerp(Quaternion.Euler(bowRestRotation), Quaternion.Euler(bowPulledRotation), pullRatio);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Arrow-Dynamic
-Unity 3D game developed for CS 410 Spring 2023. 
+
+Unity 3D game developed for CS 410 Spring 2023.
+
+# Attribution
+
+"Bow of the Pack Hunter" (https://skfb.ly/oF9DN) by Asylum Nox is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
