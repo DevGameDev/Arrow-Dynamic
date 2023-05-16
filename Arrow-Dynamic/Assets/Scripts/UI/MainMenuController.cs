a72831e63e5b1d9de32c3d3eee2021950e76a809
zacharyweisenbloom <38918997+zacharyweisenbloom@users.noreply.github.com>
2023-05-16 14:31:56 -0700

    Added main menu

--- Arrow-Dynamic/Assets/Scripts/UI/MainMenuController.cs
+++ Arrow-Dynamic/Assets/Scripts/UI/MainMenuController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+        
     }
 }
