128256e82d43413a3c1c7c660a68ad022ed62010
zacharyweisenbloom <38918997+zacharyweisenbloom@users.noreply.github.com>
2023-05-20 22:11:53 -0700

    Revert "Revert "Added Volume slider functionality""
    This reverts commit 3dd9535b622e0f0c6143c800595d3516d00574ed.

--- Arrow-Dynamic/Assets/Scripts/Managers/AudioManager.cs
+++ Arrow-Dynamic/Assets/Scripts/Managers/AudioManager.cs
@@ -15,6 +15,7 @@ public class AudioManager : MonoBehaviour
     //////////////////////////////////////////////////
     // Private Fields and Methods
     //////////////////////////////////////////////////
+
     [SerializeField] private Slider audioSlider;
     private float audioVolume = 1f; // The current audio volume
 
@@ -23,6 +24,7 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -30,23 +32,8 @@ public class AudioManager : MonoBehaviour
             return;
         }
     }
-    void Start()
-    {
-        // Set the initial volume to the value of the audio slider
-        audioVolume = audioSlider.value;
-        UpdateAudioVolume();
+    public void ChangeMasterVolume(float value){
+        AudioListener.volume = value;
     }
-    public void OnAudioSliderChanged()
-    {
-        // Called when the audio slider value changes
-        audioVolume = audioSlider.value;
-        UpdateAudioVolume();
-    }
-    private void UpdateAudioVolume()
-    {
-        // Update the audio volume for all relevant audio sources in the game
-        // Replace this code with your actual audio source management logic
-        AudioListener.volume = audioVolume;
-    }
-
 }
+    
