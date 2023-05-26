5cbd98dd9916dfdf70215e3555b8e957de7cecad
bsumser <bsumser@gmail.com>
2023-05-26 11:55:11 -0700

    basic door
    Basic door that goes up when pressure plate is collided. Needs to be extended into doors that slide or swing

---
+++ Arrow-Dynamic/Assets/Scripts/Environment/DoorTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+
+    bool isOpen = false;
+
+    void OnTriggerEnter(Collider col) {
+        if (isOpen == false) {
+            isOpen = true;
+            door.transform.position += new Vector3(0,5,0);
+        }
+    }
+}
