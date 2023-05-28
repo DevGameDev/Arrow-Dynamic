994aa66078006b2e463d9fc8bcf9abcc9aef74f7
unknown <dch@uoregon.edu>
2023-05-27 17:28:13 -0700

    base level update

---
+++ Arrow-Dynamic/Assets/Scripts/Data/EventTypes.cs
@@ -0,0 +1,12 @@
+/// <summary>
+/// An enumeration of possible game state.
+/// </summary>
+public enum EventTypes
+{
+    LoadGameStart,
+    LoadLevelTutorial,
+    LoadLevelOne,
+    LoadLevelTwo,
+    LoadLevelThree,
+    LoadLevelFinal
+}
