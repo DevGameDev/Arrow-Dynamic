42c5464dc767cad2f3ec9ae6f68fa83bd6e9f0a4
bsumser <bsumser@gmail.com>
2023-05-28 21:24:12 -0700

    audio coroutine
    added coroutine for audio fade, but isnt working

--- Arrow-Dynamic/Assets/Scripts/Environment/TargetTrigger.cs
+++ Arrow-Dynamic/Assets/Scripts/Environment/TargetTrigger.cs
@@ -19,11 +19,35 @@ public class TargetTrigger : MonoBehaviour
         isColliding = true;
         //need to give arrows tag 
         //if (col.gameObject.tag == "Arrow") {
+        
+        //StartCoroutine(Fade(false, source1, 1f, 0f));
         source1.Play();
+        
         if (isOpen == false) {
             //door1.transform.position += new Vector3(0,5,0);
             //door2.transform.position += new Vector3(0,5,0);
             isOpen = true;
         }
+    }
+    private IEnumerator Fade(bool fadeIn, AudioSource source, float duration, float targetVolume)
+    {
+        //fade audio method from SwishSwoosh https://www.youtube.com/watch?v=kYGXGDjL5jM
+        float time = 0f;
+        float startVol = source.volume;
+
+        if (!fadeIn)
+        {
+            double lengthOfSource = (double)source.clip.samples / source.clip.frequency;
+            yield return new WaitForSecondsRealtime((float)(lengthOfSource - duration));
+        }
+
+        while(time < duration)
+        {
+            time += Time.deltaTime;
+            source.volume = Mathf.Lerp(startVol, targetVolume, time / duration);
+            yield return null;
+        }
+
+        yield break;
     }
 }
