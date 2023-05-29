3dbe0082adc8f407c47e9cbbf19822aa5d725006
bsumser <bsumser@gmail.com>
2023-05-28 20:37:33 -0700

    cleanup, comments
    -cleaned up formatting and added more comments
    -door check is now a negated false bool

--- Arrow-Dynamic/Assets/Scripts/Environment/SkullFloorTrigger.cs
+++ Arrow-Dynamic/Assets/Scripts/Environment/SkullFloorTrigger.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class SkullFloorTrigger : MonoBehaviour
 {
+    //floor to be moved
     [SerializeField] GameObject floor;
+
+    //3 targets needed to shoot
     [SerializeField] GameObject target1;
     [SerializeField] GameObject target2;
     [SerializeField] GameObject target3;
+
+    //reference to bool component in skull scripts
     private TargetTrigger target1bool;
     private TargetTrigger target2bool;
     private TargetTrigger target3bool;
+
+    //vector to move the floor
     [SerializeField] float xComp;
     [SerializeField] float yComp;
     [SerializeField] float zComp;
-    [SerializeField] private float Speed = 0.5f;
-    [SerializeField] private float SlideAmount = 0.05f;
-    private Coroutine AnimationCoroutine;
-    public AudioSource doorOpenSound;
 
-    private bool doorsOpen = true;
+    //speed and slide distance to move
+    [SerializeField] private float Speed;
+    [SerializeField] private float SlideAmount;
+
+    //coroutine for the door sliding animtation
+    private Coroutine AnimationCoroutine;
+
+    private bool doorsOpen = false;
 
     private void Start()
     {
+        //get reference to bool in target scripts
         target1bool = target1.GetComponent<TargetTrigger>();
         target2bool = target2.GetComponent<TargetTrigger>();
         target3bool = target3.GetComponent<TargetTrigger>();
@@ -30,10 +41,10 @@ public class SkullFloorTrigger : MonoBehaviour
 
     private void Update()
     {
-        if (target1bool.isOpen && target2bool.isOpen && target3bool.isOpen && doorsOpen) {
+        //check to make sure all skulls active and doors are closed
+        if (target1bool.isOpen && target2bool.isOpen && target3bool.isOpen && !doorsOpen) {
             AnimationCoroutine = StartCoroutine(openDoors());
-            doorOpenSound.Play();
-            doorsOpen = false;
+            doorsOpen = true;
         }
     }
 
@@ -42,7 +53,7 @@ public class SkullFloorTrigger : MonoBehaviour
         float time = 0;
         while(time < 1)
         {
-            //new open code
+            //lerp the position from start to end
             Vector3 startPositionFloor = floor.transform.position;
             Vector3 endPositionFloor = floor.transform.position + (SlideAmount * new Vector3(xComp, yComp, zComp));
             floor.transform.position = Vector3.Lerp(startPositionFloor, endPositionFloor, time);
