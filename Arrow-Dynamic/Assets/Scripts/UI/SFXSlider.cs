2510695ef4bc8282868d21fe6eb89e45c6fce300
bsumser <bsumser@gmail.com>
2023-06-08 14:21:45 -0700

    added volume slider to settings

---
+++ Arrow-Dynamic/Assets/Scripts/UI/SFXSlider.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFXSlider : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    void Start()
+    {
+        _slider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeSFXVolume(val));
+    }
+}
